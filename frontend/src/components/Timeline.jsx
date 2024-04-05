89c7826da284d3f955c6e6b3ec75c78597789875
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-04-05 20:12:48 +0530

    dependency-issue-fixed

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import  { useRef, useState, useEffect } from "react";
 import Heading from "./Heading";
 import Section from "./Section";
 import { timeline } from "../constants";
